94759c34d42e88c7a2e12116bec953c9d96e0ebd
Amanpreet Kaur <amanpreet8507@gmail.com>
2024-02-03 09:36:34 -0800

    add post comments route

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// imports
 const express = require('express')
 const cors = require('cors')
 const app = express()
@@ -6,20 +7,19 @@ const videosRoute = require('./routes/videos')
 app.use(express.json())
 app.use(cors())
 
-// PUT the API requirement here
-// app.use((req,res,next), ()=>{
-
-// })
-
+// videosRoute
 app.use("/videos", videosRoute)
-// REQUIRED LINE OF CODE TO GET ACCESS TO process.env
+
+// dotenv and way to get PORT # from env
 require('dotenv').config()
 const { PORT } = process.env || 8000
 
+// listening to app
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`)
 })
 
+// browser check
 app.get('/', (req, res)=>{
     res.json('Hi! You are approaching to get to Videos API!!')
 })
--- routes/videos.js
+++ routes/videos.js
@@ -2,13 +2,15 @@ const express = require('express')
 const fs = require('fs')
 const router = express.Router();
 const videosData = require("../data/videos.json")
+const { v4: uuidv4 } = require('uuid');
 
-
+// get all videos with data
 router.route("/")
     .get((req, res)=> {
         res.status(200).json(videosData)
     })
 
+// get video by id
 router.route("/:id")
     .get((req, res) => {
         const {id} = req.params;
@@ -17,28 +19,40 @@ router.route("/:id")
 
         if(!videoMatch) return res.status(404).json("No videos found!");
     })
+// post video
 
-    // POST /videos/:id/comments
-    // :id must be swapped out with the numeric id of a video as found in the list of videos
-    // Creates a new comment for a specific video
-    // Post body example
-    // {
-    //               "name": "Nigel",
-    //               "comment": "This is a test"
-    //             }
-                
+
+// post comments               
 router.route("/:id/comments")
     .post((req, res) => {
-        const {comments} = req.body;
-        if(!comments.name || !comments.name) return res.status(400).json("Plese input comment")
+        const {name, comment} = req.body;
 
-        const newComment = {name, comment};
-        newComment.id = newComment.length + 1;
+        if(!name || !comment) return res.status(400).json("Plese input comment")
 
-        const freshCommentsList = JSON.parse(fs.readFileSync("../data/videos.json"))
-        freshCommentsList.push(newComment);
-        fs.writeFileSync("../data/videos.json", JSON.stringify(freshCommentsList))
-        res.status(201).json(videosData)
+        const videoId = req.params.id;
+        const freshCommentsList = JSON.parse(fs.readFileSync("./data/videos.json"))
+        
+        // Find the video
+        const video = freshCommentsList.find( video => videoId === videoId);
+        if(!video){
+            return res.status(404).json("video not found");
+        }
+
+        // new comment object
+        const newComment = {
+            id: uuidv4(),
+            name, 
+            comment,
+            likes:0,
+            timestamp: Date.now(),
+        }
+
+        video.comments.push(newComment);
+
+        // putting the new comments to the file
+        fs.writeFileSync("./data/videos.json", JSON.stringify(freshCommentsList))
+
+        res.status(201).json(video)
     })  
 
 module.exports = router;
